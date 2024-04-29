5f41ce5a85edcd6af7f4d8bfae603a8fb6a16be9
Angel Milla <angheljf@gmail.com>
2024-04-29 14:01:33 -0400

    Quartz sync: Apr 29, 2024, 2:01 PM

--- content/Scripts/Script.md
+++
@@ -1,4 +0,0 @@
-<%*
-  var tArr = tp.file.tags;
-  var tStr = tArr.join('  ');
-%><%* tR += tStr %>
--- content/Scripts/my_tags.js
+++
@@ -1,5 +0,0 @@
-function my_tags(tags) {
-    var t = tags.join('  ');
-    return t;
-}
-module.exports = my_tags;
